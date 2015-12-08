3f1fc9bba5aa53a995553dbe0924e7b13a8c4e39
Alexander <alexander.sovpel@gmail.com>
2015-12-08 21:18:24 +0300

    index with mobile and tablet views

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 
 //Загрузить больше статей/фоток, если прокрутили до конца
 window.addEventListener('scroll', function() {
+    if (!document.documentElement.scrollTop)
+        upScroll.style.display = "none";
+    else
+        upScroll.style.display = "block";
     var inProcess = false;
     var scrollHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
@@ -25,3 +29,26 @@ window.addEventListener('scroll', function() {
         }, 1000);
     }
 });
+
+
+var upScroll = document.querySelector('.up');
+upScroll.addEventListener('click', function() {
+   window.scrollTo(0, 0);
+});
+//if (document.documentElement.clientWidth <= 480) {
+//    upScroll.childNodes[1].remove();
+//}
+//else {
+//    upScroll.appendChild(document.createElement('p').innerHTML)
+//}
+
+var dropdownMobileToggle = document.querySelector('.dropdown-mobile-toggle');
+var dropdownMobile = document.querySelector('.dropdown-mobile');
+dropdownMobile.style.display = "none";
+dropdownMobileToggle.addEventListener('click', function() {
+    if (dropdownMobile.style.display == "none")
+        dropdownMobile.style.display = "block";
+    else
+        dropdownMobile.style.display = "none";
+});
+
